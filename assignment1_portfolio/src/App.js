af07e8a6215ca457c7dfb869a18069174ceadda5
mtoleran <manuelcarltoleran@gmail.com>
2024-01-28 01:59:47 -0500

    Added Services section

--- assignment1_portfolio/src/App.js
+++ assignment1_portfolio/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro";
 import About from "./components/About/about";
+import Services from "./components/Services/services";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar/>
       <Intro/>
       <About/>
+      <Services/>
     </div>
   );
 }
---
+++ assignment1_portfolio/src/components/Services/services.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './services.css';
+import webDev from '../../assets/web_development.png';
+import mobileDev from '../../assets/mobile_development.png';
+import userInterface from '../../assets/userint_userex.png';
+
+
+const Services = () => {
+    return(
+        <section id='services'>
+            <span className='servicesTitle'>Services</span>
+            <div className='serviceBars'>
+                <div className='serviceBar'>
+                    <img src={webDev} alt='' className='serviceBarImg'/>
+                    <div className='serviceBarText'>
+                        <h2>Web Development</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+                <div className='serviceBar'>
+                    <img src={mobileDev} alt='' className='serviceBarImg'/>
+                    <div className='serviceBarText'>
+                        <h2>Mobile Development</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+                <div className='serviceBar'>
+                    <img src={userInterface} alt='' className='serviceBarImg'/>
+                    <div className='serviceBarText'>
+                        <h2>UI/UX Design</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Services;
