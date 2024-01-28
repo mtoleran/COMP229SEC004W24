96987982997ab5843036bf3266042f06fdd1eefd
mtoleran <manuelcarltoleran@gmail.com>
2024-01-28 06:53:12 -0500

    Removed comment in navbar.js

--- assignment1_portfolio/src/components/NavBar/navbar.js
+++ assignment1_portfolio/src/components/NavBar/navbar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './navbar.css'
 import logo from '../../assets/logo.png';
 import contactImg from '../../assets/contact.png'
-//import {Link} from 'react-scroll';
 import {Link} from 'react-router-dom';
 
 const Navbar = () => {
