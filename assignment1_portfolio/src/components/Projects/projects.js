077f93b0bf511230cc550589857ac3108542e6d1
mtoleran <manuelcarltoleran@gmail.com>
2024-01-28 17:52:53 -0500

    Updated text in Projects page

--- assignment1_portfolio/src/components/Projects/projects.js
+++ assignment1_portfolio/src/components/Projects/projects.js
@@ -13,21 +13,30 @@ const Projects = () => {
                     <img src={tricep} alt='' className='projectBarImg'/>
                     <div className='projectBarText'>
                         <h2>TRICEP: Technologies of RFID and image recognition in a checkout enhancement platform</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p>A checkout enhancement platform which implements RFID and image recognition technologies in shopping baskets/carts, with a real-time integrated tool.
+                            I was the back-end developer for the web application  of this project built using the Django framework. The project was successful in creating 
+                            and testing a prototype of the basket cart with RFID and image scanners along with real-time tracking application. 
+                        </p>
                     </div>
                 </div>
                 <div className='projectBar'>
                     <img src={samuya} alt='' className='projectBarImg'/>
                     <div className='projectBarText'>
                         <h2>Samuya Inventory Management System</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p>An invenotry management system was developed for Samuya Food Manufacturing. I was also the back-end developer for this project.
+                            The team was successful in creating an initial version of the system.
+                        </p>
                     </div>
                 </div>
                 <div className='projectBar'>
                     <img src={inkpanda} alt='' className='projectBarImg'/>
                     <div className='projectBarText'>
                         <h2>InkPanda: Printing Delivery System</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p>The project presents the development of a mobile application that will allow people and companies to place their 
+                            printing orders online with pickup or delivery options. I served as the project manager/business analyst for this project, where I
+                            monitored the overall state of the project as well as assisted in the requirements gathering. The team was able to
+                            produce a detailed software requirements specification document including various UML diagrams, class diagrams, and mock-up screens.  
+                        </p>
                     </div>
                 </div>
             </div>
