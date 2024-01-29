c20d326b81bb5e3a27814542959332d191e3ed9b
mtoleran <manuelcarltoleran@gmail.com>
2024-01-28 20:48:59 -0500

    Updated index.js file

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
-import './public/index.css';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
